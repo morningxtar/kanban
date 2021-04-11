fd40d6c070f59aa68dfe1c400af1a0ee1039479b
Morningstar <lekouamelan@gmail.com>
2021-04-11 21:14:12 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+import { TagComponent } from './tag/tag.component';
+import {MatCardModule} from '@angular/material/card';
+import {RouterModule, Routes} from '@angular/router';
+import { BoardComponent } from './board/board.component';
+
+const appRoutes: Routes = [
+  {path: '', redirectTo: 'board', pathMatch: 'full'},
+  {path: 'board', component: BoardComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TagComponent,
+    BoardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatSliderModule,
+    RouterModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

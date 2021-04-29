6a398b51d96f5dc4fd3a9e47a7419a4d1ce6b126
maurice <mauriceaka225@gmail.com>
2021-04-29 05:50:24 +0200

    mise en forme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSliderModule} from '@angular/material/slider';
@@ -68,6 +70,7 @@ const appRoutes: Routes = [
     MatMenuModule,
     MatDialogModule,
     ReactiveFormsModule,
+    MatToolbarModule
     // TranslocoRootModule
   ],
   providers: [],
